0bfbb518ffa6e4395753f5e941818d68b4b5561b
Pauline Adam <pauline.adam@mastertp>
2015-11-13 17:13:13 +0100

    commit 4 .2

--- src/controler/controlerLocal/ChessGameControler.java
+++ src/controler/controlerLocal/ChessGameControler.java
@@ -2,31 +2,38 @@ package controler.controlerLocal;
 
 import model.Coord;
 import model.Couleur;
+import model.observable.ChessGame;
 
 public class ChessGameControler implements ChessGameControlers {
 
+	private ChessGame m_chessgame;
+	
+	public ChessGameControler(ChessGame p_chessgame) {
+		this.m_chessgame = p_chessgame;
+	}
+	
 	@Override
 	public boolean move(Coord initCoord, Coord finalCoord) {
-		// TODO Auto-generated method stub
-		return false;
+		return m_chessgame.move(initCoord.x, initCoord.y, finalCoord.x, finalCoord.y);
 	}
 
 	@Override
 	public String getMessage() {
-		// TODO Auto-generated method stub
-		return null;
+		return m_chessgame.getMessage();
 	}
 
 	@Override
 	public boolean isEnd() {
-		// TODO Auto-generated method stub
-		return false;
+		return m_chessgame.isEnd();
 	}
 
 	@Override
 	public Couleur getColorCurrentPlayer() {
-		// TODO Auto-generated method stub
-		return null;
+		return m_chessgame.getColorCurrentPlayer();
+	}
+	
+	public String toString(){
+		return m_chessgame.toString();
 	}
 
 }
--- src/model/observable/ChessGame.java
+++ src/model/observable/ChessGame.java
@@ -1,9 +1,45 @@
 package model.observable;
 
-public class ChessGame {
-	public String toString(); 
-	public boolean move (int xInit, int yInit, int xFinal, int yFinal); 
-	public boolean isEnd();
-	public String getMessage();
-	public Couleur getColorCurrentPlayer(); 
+import java.util.Observable;
+
+import model.Couleur;
+import model.Echiquier;
+
+
+public class ChessGame extends Observable{
+	
+	private Echiquier m_echiquier;
+	
+	public ChessGame(){
+		m_echiquier = new Echiquier();
+	}
+	
+	public String toString(){
+		String st = m_echiquier.toString();
+		return st;
+	}
+	
+	public boolean move (int xInit, int yInit, int xFinal, int yFinal)
+	{
+		boolean result = false;
+
+		if(m_echiquier.isMoveOk(xInit, yInit, xFinal, yFinal) && m_echiquier.move(xInit, yInit, xFinal, yFinal))//vérifie si le déplacement est possible et l'effectue
+			{
+				m_echiquier.switchJoueur(); // si le deplacement est possible, on change de joueur
+				result =true;
+			}
+		return result;
+
+	}
+	public boolean isEnd(){
+		return m_echiquier.isEnd();
+	}
+	
+	public String getMessage(){
+		return m_echiquier.getMessage();
+	}
+	
+	public Couleur getColorCurrentPlayer(){
+		return m_echiquier.getColorCurrentPlayer();
+	}
 }
