06a3db6964a212c66780e9fd4f3f0e1417908e4d
Pauline Adam <pauline.adam@mastertp>
2015-11-13 14:29:43 +0100

    1er

---
+++ src/model/Pions.java
@@ -0,0 +1,10 @@
+package model;
+
+/**
+ * @author francoise.perrin
+ * Inspiration Jacques SARAYDARYAN, Adrien GUENARD
+ * Interface sp�cifique aux d�placements des pions
+ */
+public interface Pions {
+	public boolean isMoveDiagOk(int xFinal,	int yFinal);
+}
